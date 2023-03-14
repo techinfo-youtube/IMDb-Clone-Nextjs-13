838ce071d164f8fce96dd06fb5ac89c3fc211d47
Rasheed khan <rktechnicalplus@gmail.com>
2023-03-14 15:23:54 +0530

    about page
    code till about page

---
+++ src/app/about/page.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <div className="max-w-6xl mx-auto space-y-4 p-4">
+        <h1 className="text-2xl font-medium text-amber-600">About Us</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure
+          quisquam praesentium fugit explicabo eius illo molestiae corporis,
+          atque minus nisi. Lorem ipsum dolor, sit amet consectetur adipisicing
+          elit. Iure quisquam praesentium fugit explicabo eius illo molestiae
+          corporis, atque minus nisi. Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Iure quisquam praesentium fugit explicabo eius illo
+          molestiae corporis, atque minus nisi.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure
+          quisquam praesentium fugit explicabo eius illo molestiae corporis,
+          atque minus nisi. Lorem ipsum dolor, sit amet consectetur adipisicing
+          elit. Iure quisquam praesentium fugit explicabo eius illo molestiae
+          corporis, atque minus nisi. Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Iure quisquam praesentium fugit explicabo eius illo
+          molestiae corporis, atque minus nisi.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure
+          quisquam praesentium fugit explicabo eius illo molestiae corporis,
+          atque minus nisi. Lorem ipsum dolor, sit amet consectetur adipisicing
+          elit. Iure quisquam praesentium fugit explicabo eius illo molestiae
+          corporis, atque minus nisi. Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Iure quisquam praesentium fugit explicabo eius illo
+          molestiae corporis, atque minus nisi.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure
+          quisquam praesentium fugit explicabo eius illo molestiae corporis,
+          atque minus nisi. Lorem ipsum dolor, sit amet consectetur adipisicing
+          elit. Iure quisquam praesentium fugit explicabo eius illo molestiae
+          corporis, atque minus nisi.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure
+          quisquam praesentium fugit explicabo eius illo molestiae corporis,
+          atque minus nisi.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
