88eb90b740592066f7c87f1c93745726965cd5d7
Rasheed khan <rktechnicalplus@gmail.com>
2023-03-15 11:47:02 +0530

    navbar
    complete code till navbar component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import "./globals.css";
 import Providers from "@/app/Providers";
+import Navbar from "@/components/Navbar";
 export const metadata = {
   title: "IMDB CLone",
   description: "By Techinfoyt",
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
       <body>
         <Providers>
           <Header />
+          <Navbar />
           <main>{children}</main>
         </Providers>
       </body>
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavbarItem from "./NavbarItem";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-center dark:bg-gray-600 bg-amber-100 lg:text-lg p-4">
+      <NavbarItem title={"Trending"} param="fetchTrending" />
+      <NavbarItem title={"Top Rated"} param="fetchTopRated" />
+    </div>
+  );
+};
+
+export default Navbar;
